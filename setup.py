4be8825275c2af014dac11946ff3487380f6148b
neitro <admin@rnd2.ru>
2019-01-07 21:24:48 +0300

    tmp

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='demo_package1',
-    version='0.1.8',
+    name='demo_package',
+    version='0.1.9',
     description='description',
 
     url='https://github.com/dart-neitro/demo_package1',
     author='Konstantin Neitro',
-    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     # install_requires=['requests'],
-    packages=['demo_package', 'demo_package.zzz'],
+    # packages=['demo_package', 'demo_package.zzz'],
 )
