af3acf4f2f7dbb77d3582358222297446fb59aa7
neitro <admin@rnd2.ru>
2019-01-07 20:51:48 +0300

    add foo!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='demo_package',
-    version='0.1.3',
+    version='0.1.003',
     description='description',
 
     url='https://github.com/RafaelBroseghini/OxfordAPI',
