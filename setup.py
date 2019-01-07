e25cf7853cac0426e808e567beb0c89067ec31bf
neitro <admin@rnd2.ru>
2019-01-07 20:44:24 +0300

    2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='demo_package',
-    version='0.1.001',
+    version='0.1.2',
     description='description',
 
     url='https://github.com/RafaelBroseghini/OxfordAPI',
