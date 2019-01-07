92c86d4006a528fd10e62d0051d32f92a2efeeac
neitro <admin@rnd2.ru>
2019-01-07 21:13:28 +0300

    add foo!

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='demo_package',
-    version='0.1.4',
+    version='0.1.5',
     description='description',
 
     url='https://github.com/RafaelBroseghini/OxfordAPI',
     author='Konstantin Neitro',
     # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     # install_requires=['requests'],
-    # py_modules=['foo'],
+    py_modules=['foo'],
     packages=['demo_package'],
 )
