1706822ed513661da1798a5da2f831aa47f9a601
neitro <admin@rnd2.ru>
2019-01-07 21:15:55 +0300

    tmp

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='demo_package',
-    version='0.1.5',
+    name='demo_package1',
+    version='0.1.6',
     description='description',
 
-    url='https://github.com/RafaelBroseghini/OxfordAPI',
+    url='https://github.com/dart-neitro/demo_package1',
     author='Konstantin Neitro',
     # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     # install_requires=['requests'],
     py_modules=['foo'],
-    packages=['demo_package'],
+    packages=['demo_package1'],
 )
