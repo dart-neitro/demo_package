b2aa1dd22768ba06eb9d5985cd3a199f35f8886d
neitro <admin@rnd2.ru>
2019-01-07 21:18:31 +0300

    tmp

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup
 
 setup(
     name='demo_package1',
-    version='0.1.6',
+    version='0.1.7',
     description='description',
 
     url='https://github.com/dart-neitro/demo_package1',
     author='Konstantin Neitro',
     # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     # install_requires=['requests'],
-    py_modules=['foo'],
-    packages=['demo_package1'],
+    packages=['demo_package'],
 )
