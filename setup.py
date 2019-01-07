3a4946209307ac983fd74ec40dfc4494a74748cf
neitro <admin@rnd2.ru>
2019-01-07 20:59:50 +0300

    add foo!

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(
     author='Konstantin Neitro',
     # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     # install_requires=['requests'],
-    py_modules=['foo'],
+    # py_modules=['foo'],
+    packages=['demo_package'],
 )
